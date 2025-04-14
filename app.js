e579eaf2d5e037993a187673d8574701d871f8a5
sachyy21 <shahirammarj@gmail.com>
2025-04-15 00:39:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','(your name)' ],
+			strings: [ 'HAPPY', 'BIRTHDAY!','NUR AIN NAJWA , SHAMBUYEN LOVE YOU ALWAYSSSS' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
